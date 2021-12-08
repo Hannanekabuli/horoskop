b8a07cd1d5f9307a7bfa818eb6819bd4e1738e4d
Hannane <hannanekabuli@gmail.com>
2021-12-08 13:35:30 +0100

    comment

---
+++ files/deletHoroscope.php
@@ -0,0 +1,9 @@
+<!--deleteHoroscope.php
+sidan ska bara gå att begära via DELETE,  den ska ta bort det sparade horoskopet i
+ $_SESSION och echo:a true. Om inget finns sparat ska den echo:a false.-->
+<?php 
+
+
+
+?>
+ 
---
+++ files/viewHoroscope.php
@@ -0,0 +1,3 @@
+<!--viewHoroscope.php
+sidan ska bara gå att begära via GET, den ska kolla om ett horoskop finns sparat i $_SESSION och 
+i så fall skriva ut det i output. Annars ska sidan inte skriva ut någonting.-->

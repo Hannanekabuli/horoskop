090b927137e20d12ae75dc82e653784d314c0ae0
Hannane <hannanekabuli@gmail.com>
2021-12-08 16:28:16 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,2 +1,87 @@
-function add()
-
+var horoskopList = [
+    {
+        name : "Vattumannen",
+        latinName : "Aquarius",
+        date : "21 januari - 20 februari",
+        symbol : "water-bearer",
+        element : "air",
+        planet : "Uranus"
+    },{
+        name : "Fiskarna",
+        latinName : "Pisces",
+        date : "20 februari - 20 marc",
+        symbol : "Fish",
+        element : "Water",
+        planet : "Jupiter - Neptunus"
+    },{
+        name : "Väduren",
+        latinName : "Aries",
+        date : "20 marc - 19 april",
+        symbol : "Ram",
+        element : "fire",
+        planet : "Pluto - Mars"
+    },{
+        name : "Oxen",
+        latinName : "Taurus",
+        datum : "2o april - 20 may",
+        symbol : "Ox",
+        element : "Jord",
+        planet : "Venus"
+    },{
+        name : "Tvillingarna",
+        latinName : "Gemini",
+        datum : "21 may - 20 juni",
+        symbol : "",
+        element : "air",
+        planet : "Markurius"
+    },{
+        name : "Kräften",
+        latinName : "Canser",
+        datum : "21 jni - 21 juli",
+        symbol : "crab",
+        element : "water",
+        planet : "moon"
+    },{
+        name : "Lejonet",
+        latinName : "Leo",
+        datum : "22 juli - 22 augusti",
+        symbol : "Lion",
+        element : "Fire",
+        planet : "Sun"
+    },{
+        name : "Jungfrun",
+        latinName : "Virgo",
+        datum : "23 agusti - 22 september",
+        symbol : "",
+        element : "",
+        planet : "Uranus"
+    },{
+        name : "Vågen",
+        latinName : "Libra",
+        datum : "23 september - 22 oktober",
+        symbol : "",
+        element : "",
+        planet : "Venus"
+    },{
+        name : "Skorpion",
+        latinName : "Scorpius",
+        datum : "23 oktober - 21 november",
+        symbol : "",
+        element : "water",
+        planet : "Mars"
+    },{
+        name : "Skytten",
+        latinName : "Sagittarius",
+        datum : "22 november - 21 december",
+        symbol : "",
+        element : "Fire",
+        planet : "Jupitor"
+    },{
+        name : "Stenbocken",
+        latinName : "Capricornus",
+        datum : "22 december - 20 januari",
+        symbol : "",
+        element : "",
+        planet : "Sturnus"
+    },
+];

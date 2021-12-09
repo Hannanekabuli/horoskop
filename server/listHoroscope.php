9ecedd7d01fea40d34dc407d3472459eef9bc795
Hannane <hannanekabuli@gmail.com>
2021-12-09 11:19:44 +0100

    Update listHoroscope.php

--- server/listHoroscope.php
+++ server/listHoroscope.php
@@ -6,9 +6,9 @@ $horoskopListhoroskopList = [
         "latinName" : "Aquarius",
         "start" : 0121,
         "end" : 0220,
-        "symbol" : "water-bearer",
-        "elemen"t : "air",
-        "planet" : "Uranus"
+        "symbol" : "water-Bearer",
+        "elemen"t : "Air",
+        "planet" : "Uranus -Saturn"
     ],[
         "name" : "Fiskarna",
         "latinName" : "Pisces",
@@ -23,7 +23,7 @@ $horoskopListhoroskopList = [
         "start" : 0321,
         "end" : 0420, 
         "symbol" : "Ram",
-        "element" : "fire",
+        "element" : "Fire",
         "planet" : "Pluto - Mars"
     ],[
         "name" : "Oxen",
@@ -31,15 +31,15 @@ $horoskopListhoroskopList = [
         "start" : 0421,
         "end" : 0520,
         "symbol" : "Ox",
-        "element" : "Jord",
+        "element" : "Earth",
         "planet" : "Venus"
     ],[
         "name" : "Tvillingarna",
         "latinName" : "Gemini",
         "start" : 0521,
         "end" : 0620,
-        "symbol" : "",
-        "element" : "air",
+        "symbol" : "Twin",
+        "element" : "Air",
         "planet" : "Markurius"
     ],[
         "name" : "Kräften",
@@ -47,7 +47,7 @@ $horoskopListhoroskopList = [
         "start" : 0621,
         "end" : 0721,
         "symbol" : "crab",
-        "element" : "water",
+        "element" : "Water",
         "planet" : "moon"
     ],[
         "name" : "Lejonet",
@@ -62,31 +62,31 @@ $horoskopListhoroskopList = [
         "latinName" : "Virgo",
         "start" : 0823,
         "end" : 0922,
-        "symbol" : "",
-        "element" : "",
+        "symbol" : "Maiden",
+        "element" : "Earth",
         "planet" : "Uranus"
     ],[
         "name" : "Vågen",
         "latinName" : "Libra",
         "start" : 0923,
         "end" : 1022,
-        "symbol" : "",
-        "element" : "",
+        "symbol" : "Scales",
+        "element" : "Air",
         "planet" : "Venus"
     ],[
         "name" : "Skorpion",
         "latinName" : "Scorpius",
         "start" : 1023,
         "end" : 1121,
-        "symbol" : "",
-        "element" : "water",
+        "symbol" : "Scorpion",
+        "element" : "Water",
         "planet" : "Mars"
     ],[
         "name" : "Skytten",
         "latinName" : "Sagittarius",
         "start" : 1122,
         "end" : 1221,
-        "symbol" : "",
+        "symbol" : "Archer",
         "element" : "Fire",
         "planet" : "Jupitor"
     ],[
@@ -94,8 +94,8 @@ $horoskopListhoroskopList = [
         "latinName" : "Capricornus",
         "start" : 1222,
         "end": 0120,
-        "symbol" : "",
-        "element" : "",
+        "symbol" : "Sea-Goat",
+        "element" : "Earth",
         "planet" : "Sturnus"
     ],
 ];

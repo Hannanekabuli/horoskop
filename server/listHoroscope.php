d4af29f7a234214b6b5be46dae9fabcad4e7310f
Hannane <hannanekabuli@gmail.com>
2021-12-09 10:42:40 +0100

    Update listHoroscope.php

--- server/listHoroscope.php
+++ server/listHoroscope.php
@@ -1,108 +1,107 @@
 <?php
 
-var horoskopList = [
-    {
-        name : "Vattumannen",
-        latinName : "Aquarius",
-        start : 0121,
-        end : 0220,
-        symbol : "water-bearer",
-        element : "air",
-        planet : "Uranus"
-    },{
-        name : "Fiskarna",
-        latinName : "Pisces",
-        start : 0221,
-        end : 0320,
-        symbol : "Fish",
-        element : "Water",
-        planet : "Jupiter - Neptunus"
-    },{
-        name : "Väduren",
-        latinName : "Aries",
-        start : 0321,
-        end : 0420, 
-        symbol : "Ram",
-        element : "fire",
-        planet : "Pluto - Mars"
-    },{
-        name : "Oxen",
-        latinName : "Taurus",
-        start : 0421,
-        end : 0520,
-        symbol : "Ox",
-        element : "Jord",
-        planet : "Venus"
-    },{
-        name : "Tvillingarna",
-        latinName : "Gemini",
-        star
-        t : 0521,
-        end : 0620,
-        symbol : "",
-        element : "air",
-        planet : "Markurius"
-    },{
-        name : "Kräften",
-        latinName : "Canser",
-        start : 0621,
-        end: 0721,
-        symbol : "crab",
-        element : "water",
-        planet : "moon"
-    },{
-        name : "Lejonet",
-        latinName : "Leo",
-        start : 0722,
-        end : 0822,
-        symbol : "Lion",
-        element : "Fire",
-        planet : "Sun"
-    },{
-        name : "Jungfrun",
-        latinName : "Virgo",
-        start : 0823,
-        end : 0922,
-        symbol : "",
-        element : "",
-        planet : "Uranus"
-    },{
-        name : "Vågen",
-        latinName : "Libra",
-        start: 0923,
-        end: 1022,
-        symbol : "",
-        element : "",
-        planet : "Venus"
-    },{
-        name : "Skorpion",
-        latinName : "Scorpius",
-        start : 1023,
-        end : 1121,
-        symbol : "",
-        element : "water",
-        planet : "Mars"
-    },{
-        name : "Skytten",
-        latinName : "Sagittarius",
-        start : 1122,
-        end : 1221,
-        symbol : "",
-        element : "Fire",
-        planet : "Jupitor"
-    },{
-        name : "Stenbocken",
-        latinName : "Capricornus",
-        start : 1222,
-        end : 0120,
-        symbol : "",
-        element : "",
-        planet : "Sturnus"
-    },
+$horoskopListhoroskopList = [
+    [
+        "name" => "Vattumannen",
+        "latinName" : "Aquarius",
+        "start" : 0121,
+        "end" : 0220,
+        "symbol" : "water-bearer",
+        "elemen"t : "air",
+        "planet" : "Uranus"
+    ],[
+        "name" : "Fiskarna",
+        "latinName" : "Pisces",
+        "start" : 0221,
+        "end" : 0320,
+        "symbol" : "Fish",
+        "element" : "Water",
+        "planet" : "Jupiter - Neptunus"
+    ],[
+        "name" : "Väduren",
+        "latinName" : "Aries",
+        "start" : 0321,
+        "end" : 0420, 
+        "symbol" : "Ram",
+        "element" : "fire",
+        "planet" : "Pluto - Mars"
+    ],[
+        "name" : "Oxen",
+        "latinName" : "Taurus",
+        "start" : 0421,
+        "end" : 0520,
+        "symbol" : "Ox",
+        "element" : "Jord",
+        "planet" : "Venus"
+    ],[
+        "name" : "Tvillingarna",
+        "latinName" : "Gemini",
+        "start" : 0521,
+        "end" : 0620,
+        "symbol" : "",
+        "element" : "air",
+        "planet" : "Markurius"
+    ],[
+        "name" : "Kräften",
+        "latinName" : "Canser",
+        "start" : 0621,
+        "end" : 0721,
+        "symbol" : "crab",
+        "element" : "water",
+        "planet" : "moon"
+    ],[
+        "name" : "Lejonet",
+        "latinName" : "Leo",
+        "start" : 0722,
+        "end" : 0822,
+        "symbol" : "Lion",
+        "element" : "Fire",
+        "planet" : "Sun"
+    ],[
+        "name" : "Jungfrun",
+        "latinName" : "Virgo",
+        "start" : 0823,
+        "end" : 0922,
+        "symbol" : "",
+        "element" : "",
+        "planet" : "Uranus"
+    ],[
+        "name" : "Vågen",
+        "latinName" : "Libra",
+        "start" : 0923,
+        "end" : 1022,
+        "symbol" : "",
+        "element" : "",
+        "planet" : "Venus"
+    ],[
+        "name" : "Skorpion",
+        "latinName" : "Scorpius",
+        "start" : 1023,
+        "end" : 1121,
+        "symbol" : "",
+        "element" : "water",
+        "planet" : "Mars"
+    ],[
+        "name" : "Skytten",
+        "latinName" : "Sagittarius",
+        "start" : 1122,
+        "end" : 1221,
+        "symbol" : "",
+        "element" : "Fire",
+        "planet" : "Jupitor"
+    ],[
+        "name" : "Stenbocken",
+        "latinName" : "Capricornus",
+        "start" : 1222,
+        "end": 0120,
+        "symbol" : "",
+        "element" : "",
+        "planet" : "Sturnus"
+    ],
 ];
 
 function horoskopList($date) {
-    if($date >= 0121 && $date <= 0220)
+    if($date >= 0121 && $date <= 0220){
 
     return "Vattumannen";
 }elseif($date >= 0221 && $date <= 0320){
@@ -137,9 +136,9 @@ return "Fiskarna";
     return "Skytten";
 }else($date >= 1222 && $date <= 0120){
 
-    return "Stenbocken";
+    return "Stenbocken";}
 
-    
+
 
 
 

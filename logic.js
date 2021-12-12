7af48f7d76aaa6df2b947155351a722ca1699f93
Hannane <hannanekabuli@gmail.com>
2021-12-12 10:35:50 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -84,4 +84,19 @@ function deleteResult() {
 }
 
 
+function makeRequest(url, method, formData, callback){
+ 
+    fetch(url, {
+        method: method,
+        body: formData
+    }).then((response) => {
+        return response.json()
+    }).then((result) => {
+        console.log(result)
+        callback(result)
+    }).catch((err) => {
+        console.log("Error : ",err); 
+    })
+}
+
 

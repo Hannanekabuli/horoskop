06106ce1ea604d96849b4f70c168e7f22fde7db9
Hannane <hannanekabuli@gmail.com>
2021-12-13 23:47:56 +0100

    Update deletHoroscope.php

--- server/deletHoroscope.php
+++ server/deletHoroscope.php
@@ -2,29 +2,37 @@
 sidan ska bara gå att begära via DELETE,  den ska ta bort det sparade horoskopet i
  $_SESSION och echo:a true. Om inget finns sparat ska den echo:a false.-->
 
- <?php
-session_start();
 
-if(isset($_SERVER['REQUEST_METHOD'])) {
+ <?php 
 
-    if($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+try {
+        session_start();
 
-        if (isset($_SESSION["horoscope"])) {
-            
-            unset($_SESSION['horoscope']);
-            echo json_encode(true);
+        if ($_SERVER['REQUEST_METHOD']) {
 
+                if($_SERVER["REQUEST_METHOD"] == "DELETE") {
+                
+                        if(isset($_SESSION["horoscope"])) {
+                                session_destroy();
+
+                                echo json_encode(true);
+                                exit;
+                        } else {
+                                echo json_encode(false);
+                                exit;
+                        }
+
+                } else {
+                        throw new Exception("Endpoint not valid", 405);
+                }
 
         } else {
-                
-            echo json_encode(false);
+                throw new Exception("http-request not sent", 404);
         }
-    } else {
-        echo json_encode("not a Delete method");
-    }
-} else {
 
-    echo json_encode("No valid request");
+} catch(Exception $err) {
+        http_response_code($err -> getCode());
+        echo json_encode($err -> getMessage()); 
+        exit;
 }
 ?>
- 

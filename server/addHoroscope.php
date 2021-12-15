8e434408ac04f9f9c1cf8c5f060d0f5f7d95a1c6
Hannane <hannanekabuli@gmail.com>
2021-12-15 21:59:48 +0100

    Update addHoroscope.php

--- server/addHoroscope.php
+++ server/addHoroscope.php
@@ -1,12 +1,6 @@
-<!--addHoroscope.php
-sidan ska bara gå att begära via POST,  den ska kolla efter ett födelsedatum i $_POST, räkna ut vilket horoskop födelsedatumet tillhör och spara det i $_SESSION.
-Om ett horoskop redan finns sparat ska det inte skrivas över. Om det inte gick att räkna ut horoskopet ska ingenting sparas.
-Sidan ska inte använda echo eller skriva någon output förutom true eller false, beroende på om horoskopet sparades.-->
-
 <?php 
 
 require('./listHoroscope.php');
-
 session_start();
 
 if(isset($_SERVER['REQUEST_METHOD'])) {
@@ -21,7 +15,6 @@ if(isset($_SERVER['REQUEST_METHOD'])) {
 
             echo json_encode(true);
             exit;
-            
         } else {
             echo json_encode(false);
             exit;

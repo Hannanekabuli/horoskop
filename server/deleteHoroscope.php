9095e774c98a9b72e2b788f4c1da35c4ed5e3450
Hannane <hannanekabuli@gmail.com>
2021-12-17 21:59:56 +0100

    update

--- server/deleteHoroscope.php
+++ server/deleteHoroscope.php
@@ -8,7 +8,7 @@ if(isset($_SERVER['REQUEST_METHOD'])) {
 
         if(isset($_SESSION["horoscope"])) {
 
-            unset($_SESSION["horoscope"]);
+            session_destroy()
 
             echo json_encode(true);
             exit;
@@ -16,6 +16,11 @@ if(isset($_SERVER['REQUEST_METHOD'])) {
             echo json_encode(false);
             exit;
         }
+
+        
+    } else {
+        throw new Exception("http-request not sent", 404);
+    }
 
     }else { 
         echo json_encode("not a POST method");
